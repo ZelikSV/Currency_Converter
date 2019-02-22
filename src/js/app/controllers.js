77cc6917105d3e1d34135840ebd6499071583372
Serhii <zeliksv@gmail.com>
2019-02-22 17:02:00 +0200

    addAngular, add controller with functions

--- src/js/app/controllers.js
+++ src/js/app/controllers.js
@@ -1,11 +1,31 @@
-myApp.controller('myController', function($scope, $http) {
-    $scope.activeTab = true;
-    $scope.countVal = 0;
-    $scope.costVal = 0;
-    $scope.model = [];
-    $http.get('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
-      .then(response => { response.data.forEach(item => {
-        $scope.model.push(item.ccy);
-      }); });
-  });
-  
+myApp.controller('myController', ['$scope', 'getCurrency', 'currencyService', function($scope, getCurrency, currencyService) {
+  $scope.activeTab = true;
+  $scope.countVal = null;
+  $scope.costVal = null;
+  $scope.currency = getCurrency.loadCache();
+  $scope.currencyFrom = 'USD';
+  $scope.currencyTo = 'EUR';
+
+  $scope.changeValues = () => {
+    [$scope.countVal, $scope.costVal] = [$scope.costVal, $scope.countVal];
+    [$scope.currencyFrom, $scope.currencyTo] = [$scope.currencyTo, $scope.currencyFrom];
+  };
+
+  $scope.convertValue = () => {
+    let res = 0;
+    $scope.currency.forEach(item => {
+      if (item.ccy === $scope.currencyFrom) {
+        res = currencyService.convertToUa($scope.countVal, item.buy);
+      }
+    });
+
+    $scope.currency.forEach(item => {
+      if (item.ccy === $scope.currencyTo) {
+        $scope.costVal = currencyService.convertFromUa(res, item.sale);
+      }
+    });
+  };
+  $scope.addCommissions = e => {
+    $scope.costVal -= currencyService.addCommission($scope.costVal, e.target.value);
+  };
+}]);
