b7125d137efac23b4b966b6458211035e0f9898c
Serhii <zeliksv@gmail.com>
2019-03-05 10:26:49 +0200

    smallApp, add footer and header

--- src/app/index.components.js
+++ src/app/index.components.js
@@ -1,6 +1,10 @@
-
 import currencyModule from './components/currency/currency.module';
+import headerModule from './components/header/header.module';
+import footerModule from './components/footer/footer.module';
+
 
 export default angular.module('index.components', [
-	currencyModule.name
+	currencyModule.name,
+	headerModule.name,
+	footerModule.name
 ]);
